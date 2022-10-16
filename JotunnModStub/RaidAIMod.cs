62771afa858081beea6f253133783b8bf733db27
Tyler Cai <torkyguy@gmail.com>
2022-10-15 21:25:28 -0700

    Minor renaming and refactoring

--- JotunnModStub/RaidAIMod.cs
+++ JotunnModStub/RaidAIMod.cs
@@ -1,22 +1,17 @@
-﻿// JotunnModStub
-// a Valheim mod skeleton using Jötunn
-// 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
-
-using BepInEx;
+﻿using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
+using Jotunn.Utils;
 
 namespace JotunnModStub
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
-    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class RaidAIMod : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "torky.RaidAI";
+        public const string PluginName = "RaidAI";
         public const string PluginVersion = "0.0.1";
         
         // Use this class to add your own localization to the game
@@ -26,10 +21,11 @@ namespace JotunnModStub
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
-            
+            Jotunn.Logger.LogInfo("Raid AI starting up...");
+
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+            Jotunn.Logger.LogInfo("Raid AI setup finished...");
         }
     }
 }
