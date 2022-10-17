f97498321f54b7165e199281bcd2ef7a56ef0fe4
Tyler Cai <torkyguy@gmail.com>
2022-10-16 18:32:00 -0700

    First successful import

--- ValheimRaids/ValheimRaidsMod.cs
+++ ValheimRaids/ValheimRaidsMod.cs
@@ -1,7 +1,10 @@
 ﻿using BepInEx;
+using Jotunn.Configs;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
+using System.IO;
+using UnityEngine;
 
 namespace JotunnModStub
 {
@@ -12,7 +15,7 @@ namespace JotunnModStub
     {
         public const string PluginGUID = "torky.RaidAI";
         public const string PluginName = "RaidAI";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginVersion = "0.0.2";
         
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
@@ -22,9 +25,18 @@ namespace JotunnModStub
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("Raid AI starting up...");
-
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+            var directoryPath = Path.GetDirectoryName(typeof(RaidAIMod).Assembly.Location);
+            var test = AssetUtils.LoadAssetBundle($"{directoryPath}/Assets/AssetBundles/raid");
+            Jotunn.Logger.LogInfo(test.name);
+            Jotunn.Logger.LogInfo(string.Join("", test.GetAllAssetNames()));
+            var testGameObject = test.LoadAsset<GameObject>("assets/gameobject/raidgreydwarf.prefab");
+            Jotunn.Logger.LogInfo(testGameObject.name);
+            CreatureConfig creatureConfig = new CreatureConfig
+            {
+                Name = testGameObject.name
+            };
+            CustomCreature cc = new CustomCreature(testGameObject, true, creatureConfig);
+            CreatureManager.Instance.AddCreature(cc);
             Jotunn.Logger.LogInfo("Raid AI setup finished...");
         }
     }
